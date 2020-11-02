24f03f2840ac71ed99b7e6f74eae06c0a2af9390
Yuna Blum <yuna.blum@macbook-pro-de-maccityuna.home>
2020-11-02 23:24:20 +0100

    add immune method codes

---
+++ meteor_files/algorithm_MT11.R
@@ -0,0 +1,42 @@
+program <-
+function(D_matrix, k=NULL, simcan=NULL) {
+  ##
+  ## YOUR CODE BEGINS HERE
+  ##
+  
+  if ( !{ "immunedeconv" %in% installed.packages( ) } ) {
+    if (!requireNamespace("remotes", quietly = TRUE))
+      install.packages("remotes")
+    remotes::install_github("icbi-lab/immunedeconv")
+  }
+  
+  library(immunedeconv)
+  
+  ## we compute the estimation of A for the data set :
+  A_matrix <- NULL
+  if ( !is.null(x = D_matrix) ) {
+    ## temporary hack : we keep only the 10 000 first lines in order to save time for the baseline
+    if ( nrow(x = D_matrix) > 1e4 ) {
+      D_matrix[seq_len(length.out = 1e4), ]
+    }
+    
+    res <- immunedeconv::deconvolute(gene_expression = D_matrix, 
+                                     method = "quantiseq",
+                                     tumor = TRUE, 
+                                     arrays = FALSE,
+                                     scale_mrna = TRUE)
+    
+    res = data.frame(res, row.names = 1)
+    rownames(res) = paste0(rownames(res),"|","quanTIseq")
+    
+    A_matrix = res
+    T_matrix = NULL
+  }
+  
+  ##
+  ## YOUR CODE ENDS HERE
+  ##
+  
+  return( list(A_matrix = A_matrix, T_matrix = T_matrix) )
+  
+}
---
+++ meteor_files/algorithm_MT8.R
@@ -0,0 +1,43 @@
+program <-
+function(D_matrix, k=NULL, simcan=NULL) {
+  ##
+  ## YOUR CODE BEGINS HERE
+  ##
+  
+  if ( !{ "immunedeconv" %in% installed.packages( ) } ) {
+    if (!requireNamespace("remotes", quietly = TRUE))
+      install.packages("remotes")
+    remotes::install_github("icbi-lab/immunedeconv")
+  }
+  
+  library(immunedeconv)
+  
+  # Provide CIBERSORT files
+  set_cibersort_binary("/input/CIBERSORT.R")
+  set_cibersort_mat("/input/LM22.txt")
+  
+  ## we compute the estimation of A for the data set :
+  A_matrix <- NULL
+  if ( !is.null(x = D_matrix) ) {
+    ## temporary hack : we keep only the 10 000 first lines in order to save time for the baseline
+    if ( nrow(x = D_matrix) > 1e4 ) {
+      D_matrix[seq_len(length.out = 1e4), ]
+    }
+    
+    res <- immunedeconv::deconvolute(gene_expression = D_matrix,
+                                     method = "cibersort",
+                                     arrays = FALSE)
+    res = data.frame(res, row.names = 1)
+    rownames(res) = paste0(rownames(res),"|","CIBERSORT")
+    
+    A_matrix = res
+    T_matrix = NULL
+  }
+  
+  ##
+  ## YOUR CODE ENDS HERE
+  ##
+  
+  return( list(A_matrix = A_matrix, T_matrix = T_matrix) )
+  
+}
